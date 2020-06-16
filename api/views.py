61242727f080cfdbe52dc3c1b4566c579efd7c51
k.puchkov <U098iolk>
2020-06-17 02:59:08 +0300

    disable permissions add django filters add pagination add path url

--- api/views.py
+++ api/views.py
@@ -1,24 +1,26 @@
 from .models import Category, Genre, Title
 from .permissions import IsAdminOrReadOnly
 from .serializers import CategorySerializer, GenreSerializer
-from rest_framework.viewsets import ModelViewSet
 from rest_framework import filters, viewsets
 
 
-class CategoryViewSet(viewsets.GenericViewSet):
-
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAdminOrReadOnly, ]
+    # permission_classes = [IsAdminOrReadOnly, ]
     lookup_field = 'slug'
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', ]
 
 
-class GenreViewSet(viewsets.GenericViewSet):
+class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [IsAdminOrReadOnly, ]
+    # permission_classes = [IsAdminOrReadOnly, ]
     lookup_field = 'slug'
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', ]
+
+
+class TitleViewSet(viewsets.GenericViewSet):
+    pass
--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+ }
