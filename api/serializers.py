8d25863c0cf7ed4b73a4e635186217db7fc20bd3
k.puchkov <U098iolk>
2020-06-17 23:53:42 +0300

    add django filters add titles view set add titles route add titles serializers add title filter

--- api/serializers.py
+++ api/serializers.py
@@ -14,3 +14,22 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('name', 'slug')
         model = Genre
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(many=True, read_only=True)
+    rating = serializers.FloatField(required=False)
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'rating', 'description', 'genre', 'category')
+        model = Title
+
+
+class TitleWriteSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='slug', queryset=Category.objects.all(), required=False)
+    genre = serializers.SlugRelatedField(slug_field='slug', queryset=Genre.objects.all(), many=True, required=False)
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        model = Title
--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from .models import Category, Genre, Title
+from .filters import TitleFilter
 from .permissions import IsAdminOrReadOnly
-from .serializers import CategorySerializer, GenreSerializer
+from .serializers import CategorySerializer, GenreSerializer, TitleReadSerializer, TitleWriteSerializer
 from rest_framework import filters, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -22,5 +24,13 @@ class GenreViewSet(viewsets.ModelViewSet):
     search_fields = ['name', ]
 
 
-class TitleViewSet(viewsets.GenericViewSet):
-    pass
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    # permission_classes = [IsAdminOrReadOnly,]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleReadSerializer
+        return TitleWriteSerializer
