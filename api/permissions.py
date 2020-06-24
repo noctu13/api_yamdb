3f0bda8084cfc698393958570e36b89e3dd41684
Monetka <42166073+noctu13@users.noreply.github.com>
2020-06-24 23:02:42 +0300

    Update permissions.py
    -black fix

--- api/permissions.py
+++ api/permissions.py
@@ -7,19 +7,33 @@ class IsReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
 
+
 class IsAdminClient(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated
-                    and request.user.role == Role.ADMIN)
+        return bool(
+            request.user
+            and request.user.is_authenticated
+            and request.user.role == Role.ADMIN
+        )
+
 
 class IsModeratorClient(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated
-                    and request.user.role == Role.MODERATOR)
+        return bool(
+            request.user
+            and request.user.is_authenticated
+            and request.user.role == Role.MODERATOR
+        )
+
 
 class IsAuthorOrStaff(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(request.user and request.user.is_authenticated
-                    and bool(request.user == obj.author
-                    or request.user.role == Role.MODERATOR
-                    or request.user.role == Role.ADMIN))
+        return bool(
+            request.user
+            and request.user.is_authenticated
+            and bool(
+                request.user == obj.author
+                or request.user.role == Role.MODERATOR
+                or request.user.role == Role.ADMIN
+            )
+        )
